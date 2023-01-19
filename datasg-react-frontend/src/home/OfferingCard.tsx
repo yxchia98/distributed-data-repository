0e37b5ad8317e2823b7bb49fd5b5c021be4135f5
lulmao <yxchia98@gmail.com>
2023-01-20 01:06:21 +0800

    done up offering card

--- datasg-react-frontend/src/home/OfferingCard.tsx
+++ datasg-react-frontend/src/home/OfferingCard.tsx
@@ -1,5 +1,8 @@
 // import { regular, solid } from "@fortawesome/fontawesome-svg-core";
-import { FiShare2 } from "react-icons/fi";
+import { IconContext } from "react-icons";
+import { BsFillArchiveFill } from "react-icons/bs";
+import { BiShareAlt } from "react-icons/bi";
+import { MdLockOpen } from "react-icons/md";
 
 const OfferingCard = () => {
     return (
@@ -10,12 +13,46 @@ const OfferingCard = () => {
                 </div>
             </div>
 
-            <div className="flex justify-center">
-                <div className="items-center">
-                    <FiShare2 /> <p>Data Archiving</p>
-                    <p>Safely archive data for auditing purposes</p>
+            <div className="flex justify-center items-center">
+                <div className="w-[30%] flex flex-col items-center">
+                    <div className=" bg-purple-200 rounded-xl text-center my-4">
+                        <IconContext.Provider value={{ size: "2.5em", color: "rgb(85, 33, 181)" }}>
+                            <div className="p-4">
+                                <BsFillArchiveFill />
+                            </div>
+                        </IconContext.Provider>
+                    </div>
+                    <div className="font-semibold text-gray-700 2xl:text-2xl xl:text-xl md:text-lg sm:text-md">
+                        Data Archiving
+                    </div>
+                    <div className=" text-gray-700">Safely archive data for auditing purposes</div>
                 </div>
-                <div className="flex-h justify-center items-center h-20">Hello</div>
+                <div className="w-[30%] flex flex-col items-center">
+                    <div className=" bg-green-200 rounded-xl text-center my-4">
+                        <IconContext.Provider value={{ size: "2.5em", color: "rgb(3 84 63)" }}>
+                            <div className="p-4">
+                                <BiShareAlt />
+                            </div>
+                        </IconContext.Provider>
+                    </div>
+                    <div className="font-semibold text-gray-700 2xl:text-2xl xl:text-xl md:text-lg sm:text-md">
+                        Data Sharing
+                    </div>
+                    <div className=" text-gray-700">Efficiently collaborate and share data</div>
+                </div>
+                <div className="w-[30%] flex flex-col items-center">
+                    <div className=" bg-pink-200 rounded-xl text-center my-4">
+                        <IconContext.Provider value={{ size: "2.5em", color: "rgb(155, 28, 28)" }}>
+                            <div className="p-4">
+                                <MdLockOpen />
+                            </div>
+                        </IconContext.Provider>
+                    </div>
+                    <div className="font-semibold text-gray-700 2xl:text-2xl xl:text-xl md:text-lg sm:text-md">
+                        Access Control
+                    </div>
+                    <div className=" text-gray-700">Manage data access securely</div>
+                </div>
             </div>
         </div>
     );
